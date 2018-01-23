95aa5a2d0761d48722de96ef2753d4e923778e3b
Fridakhalo <aj050630@gmail.com>
2018-01-23 12:34:33 +0100

    Include Mustache

--- js/app.js
+++ js/app.js
@@ -7,16 +7,20 @@
       .then(function(data){
       console.log(data);
 
-             $('.name').append("<p>"+data.name +"</p>");
-             $('.birth_year').append("<p>"+data.birth_year +"</p>");
-             $('.height').append("<p>"+data.height +"</p>");
-             $('.mass').append("<p>"+data.mass +"</p>");
-             $('.hair_color').append("<p>"+data.hair_color +"</p>");
-             $('.skin_color').append("<p>"+data.skin_color +"</p>");
-             $('.eye_color').append("<p>"+data.eye_color +"</p>");
-             $('.gender').append("<p>"+data.gender +"</p>");
+             // $('.name').append("<p>"+data.name +"</p>");
+             // $('.birth_year').append("<p>"+data.birth_year +"</p>");
+             // $('.height').append("<p>"+data.height +"</p>");
+             // $('.mass').append("<p>"+data.mass +"</p>");
+             // $('.hair_color').append("<p>"+data.hair_color +"</p>");
+             // $('.skin_color').append("<p>"+data.skin_color +"</p>");
+             // $('.eye_color').append("<p>"+data.eye_color +"</p>");
+             // $('.gender').append("<p>"+data.gender +"</p>");
 
-             
+        var template=$('#template').html();
+        Mustache.parse();
+        var rendered=Mustache.render(template, {result:data});
+        $("#target").append(rendered);
+
 
      });
 
