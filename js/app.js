3003cb00e66b0c535a021d56dbcb60974068b041
Fridakhalo <aj050630@gmail.com>
2018-01-23 10:58:09 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+(function main(){
+
+ var lien ="https://swapi.co/api/people/1";
+
+      $.getJSON(lien)
+      .then(function(data){
+      console.log(data);
+
+             $('.name').append("<p>"+data.name +"</p>");
+             $('.birth_year').append("<p>"+data.birth_year +"</p>");
+             $('.height').append("<p>"+data.height +"</p>");
+             $('.mass').append("<p>"+data.mass +"</p>");
+             $('.hair_color').append("<p>"+data.hair_color +"</p>");
+             $('.skin_color').append("<p>"+data.skin_color +"</p>");
+             $('.eye_color').append("<p>"+data.eye_color +"</p>");
+             $('.gender').append("<p>"+data.gender +"</p>");
+
+             
+
+     });
+
+})();
+
+
